6b0a7c62baf1992015eddfe0b77fecfaab777087
Aryantiwarib <143889805+Aryantiwarib@users.noreply.github.com>
2024-12-08 15:25:55 +0530

    Add files via upload

---
+++ Home-Service-web-app/next.config.mjs
@@ -0,0 +1,15 @@
+// export default {
+//     images: {
+//       domains: ['media.graphassets.com'],
+//     },
+//   };
+  
+
+
+
+
+export default {
+  images: {
+    domains: ['media.graphassets.com', 'lh3.googleusercontent.com'],
+  },
+};
